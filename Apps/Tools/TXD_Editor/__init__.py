2c1c20021a67a6fcd079ce5d6bf713525b5166c7
X-Seti <keithvc1972@hotmail.com>
2025-09-12 19:49:07 +0100

    purge

--- Apps/Tools/TXD_Editor/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-TXD Editor module for the GTA Renderware Modding Suite
-"""
-
-from .TXD_Editor import TXDEditorTool
-
-__all__ = ["TXDEditorTool"]
--- To_port/IMG_Editor/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-IMG Editor package
-Provides tools for editing GTA IMG archive files.
-"""
-
-from application.tools.IMG_Editor.img_editor_tool import ImgEditorTool
-from application.tools.IMG_Editor.img_controller import IMGController
-
-__all__ = ['ImgEditorTool', 'IMGController']
