048f09b9dca8269ab1954d391ec337b20e04b407
X-Seti <keithvc1972@hotmail.com>
2025-09-13 14:44:14 +0100

    refresh

--- To_port/main.py
+++
@@ -1,9 +0,0 @@
-"""
-Renderware Modding Suite - Main Entry Point
-A modern modding suite for GTA 3D era games - Modular Version
-"""
-
-from application.main_application import main
-
-if __name__ == "__main__":
-    main()
